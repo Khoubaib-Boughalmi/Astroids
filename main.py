1c65d45c2c3edc51940429ed76256dc395fe5b49
khoubaib boughalmi <khoubaib.kb99@gmail.com>
2024-09-12 13:01:22 +0100

    added fps restriction

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 
 def main():
     pygame.init()
+    dt = 0
+    fps = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while(1):
         for event in pygame.event.get():
@@ -11,6 +13,7 @@ def main():
 
         pygame.Surface.fill(screen, color=0)
         pygame.display.flip()
+        dt = fps.tick(60) / 1000 # amount of time that has passed since last call in ms
 
 
 if __name__ == "__main__":
