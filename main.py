89ebc60960f0f7569094166cb472f81369d6796a
khoubaib boughalmi <khoubaib.kb99@gmail.com>
2024-09-12 13:22:42 +0100

    added static player body

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from constants import *
+from player import Player
 import pygame
 
 def main():
@@ -6,12 +7,13 @@ def main():
     dt = 0
     fps = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while(1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
         pygame.Surface.fill(screen, color=0)
+        player.draw(screen)
         pygame.display.flip()
         dt = fps.tick(60) / 1000 # amount of time that has passed since last call in ms
 
